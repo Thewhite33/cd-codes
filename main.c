90e8838131c1c05c464db4bd9a59fa06c2445460
TheWhite33 <rohitrc2005@gmail.com>
2026-06-03 20:35:33 +0530

    arrows added

--- main.c
+++ main.c
@@ -1,19 +1,60 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
 #include<ctype.h>
+#include<string.h>
 #define MAX 100
 
 char prod[MAX][MAX];
+int n;
 char first[MAX][MAX];
 char follow[MAX][MAX];
 char table[MAX][MAX][MAX];
 char nt[MAX];
-char terms[MAX];
-
-int ntCount = 0;
+char start;
+char term[MAX];
 int termCount = 0;
-int n;
+int ntCount = 0;
+
+int ntIdx(char c){
+    for(int i=0;i<ntCount;i++){
+        if(nt[i] == c){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int tIdx(char c){
+    for(int i=0;i<termCount;i++){
+        if(term[i] == c){
+            return i;
+        }
+    }
+    return -1;
+}
+
+void getTerminals(){
+    for(int i=0;i<n;i++){
+        for(int j=3;prod[i][j];j++){
+            if(!isupper(prod[i][j]) && prod[i][j] != '#'){
+                if(tIdx(prod[i][j]) == -1){
+                    term[termCount++] = prod[i][j];
+                }
+            }
+        }
+    }
+    if(tIdx('$') == -1){
+        term[termCount++] = '$';
+    }
+}
+
+void getNonTerminals(){
+    for(int i=0;i<n;i++){
+        if(ntIdx(prod[i][0]) == -1){
+            nt[ntCount++] = prod[i][0];
+        }
+    }
+}
 
 int contains(char set[],char c){
     for(int i=0;set[i];i++){
@@ -34,40 +75,14 @@ int add(char set[],char c){
     return 0;
 }
 
-int ntIdx(char c){
-    for(int i=0;nt[i];i++){
-        if(nt[i] == c){
-            return i;
-        }
-    }
-    return -1;
-}
-
-int tIdx(char c){
-    for(int i=0;terms[i];i++){
-        if(terms[i] == c){
-            return i;
-        }
-    }
-    return -1;
-}
-
-void getNonTerminals(){
-    for(int i=0;i<n;i++){
-        char c = prod[i][0];
-        if(ntIdx(c) == -1){
-            nt[ntCount++] = c;
-        }
-    }
-}
-
 void computeFirst(){
     for(int i=0;i<n;i++){
         char A = prod[i][0];
-        char X = prod[i][2];
-        if(!isupper(X)){
+        char B = prod[i][3];
+
+        if(!isupper(B)){
             int Aidx = ntIdx(A);
-            add(first[Aidx],X);
+            add(first[Aidx],B);
         }
     }
     int changed = 1;
@@ -76,18 +91,19 @@ void computeFirst(){
         for(int i=0;i<n;i++){
             char A = prod[i][0];
             int Aidx = ntIdx(A);
-            char *rhs = prod[i]+2;
+
+            char *rhs = prod[i]+3;
             int hasEps = 1;
             for(int m=0;rhs[m] && hasEps;m++){
                 hasEps = 0;
                 char X = rhs[m];
+                int Xidx = ntIdx(X);
                 if(isupper(X)){
-                    int Xidx = ntIdx(X);
-                    for(int j=0;first[Xidx][j];j++){
-                        if(first[Xidx][j] == '#'){
+                    for(int l=0;first[Xidx][l];l++){
+                        if(first[Xidx][l] == '#'){
                             hasEps = 1;
                         }else{
-                            if(add(first[Aidx],first[Xidx][j])){
+                            if(add(first[Aidx],first[Xidx][l])){
                                 changed = 1;
                             }
                         }
@@ -130,12 +146,12 @@ void computeFollow(){
             char A = prod[i][0];
             int Aidx = ntIdx(A);
 
-            for(int j=2;prod[i][j]!='\0';j++){
+            for(int j=3;prod[i][j];j++){
                 char X = prod[i][j];
                 if(isupper(X)){
                     int Xidx = ntIdx(X);
-                    int k = j+1;
                     int hasEps = 1;
+                    int k = j+1;
                     while(prod[i][k] != '\0' && hasEps){
                         hasEps = 0;
                         char B = prod[i][k];
@@ -162,8 +178,8 @@ void computeFollow(){
                         k++;
                     }
                     if(hasEps){
-                        for(int l=0;follow[Aidx][l];l++){
-                            if(add(follow[Xidx],follow[Aidx][l])){
+                        for(int f=0;follow[Aidx][f];f++){
+                            if(add(follow[Xidx],follow[Aidx][f])){
                                 changed = 1;
                             }
                         }
@@ -184,34 +200,19 @@ void printFollow(){
     }
 }
 
-// LL(1) table and stack trace functions 
-
-void getTerminals(){
-    for(int i=0;i<n;i++){
-        for(int j=2;prod[i][j] != '\0';j++){
-            if(!isupper(prod[i][j]) && prod[i][j] != '#'){
-                if(tIdx(prod[i][j]) == -1){
-                    terms[termCount++] = prod[i][j];
-                }
+void tableInput(){
+    printf("Enter table entries: \n");
+    for(int i=0;i<ntCount;i++){
+        for(int j=0;j<termCount;j++){
+            printf("M[%c,%c] = ",nt[i],term[j]);
+            char temp[MAX];
+            scanf("%s",temp);
+            if(strcmp(temp,"-") == 0){
+                table[i][j][0] = '\0';
+            }else{
+                strcpy(table[i][j],temp);
             }
         }
-    }
-    if(tIdx('$') == -1){
-        terms[termCount++] = '$';
-    }
-}
-
-void takeFirst(){
-    for(int i=0;i<ntCount;i++){
-        printf("first(%c) = ",nt[i]);
-        scanf("%s",first[i]);
-    }
-}
-
-void takeFollow(){
-    for(int i=0;i<ntCount;i++){
-        printf("follow(%c) = ",nt[i]);
-        scanf("%s",follow[i]);
     }
 }
 
@@ -224,23 +225,21 @@ void buildTable(){
 
     for(int i=0;i<ntCount;i++){
         char A = nt[i];
+
         for(int j=0;j<n;j++){
             if(prod[j][0] != A) continue;
 
-            char *rhs = prod[j]+2;
+            char *rhs = prod[j]+3;
             int rhsCanbeEps = 1;
-
             for(int m=0;rhs[m] && rhsCanbeEps;m++){
-                char X = rhs[m];
                 rhsCanbeEps = 0;
-
+                char X = rhs[m];
                 if(isupper(X)){
-                    int hasEps = 0;
                     int Xidx = ntIdx(X);
-                    for(int k=0;first[Xidx][k];k++){
-                        char firstSym = first[Xidx][k];
-                        if(firstSym != '#'){
-                            int ti = tIdx(firstSym);
+                    int hasEps = 0;
+                    for(int l=0;first[Xidx][l];l++){
+                        if(first[Xidx][l] != '#'){
+                            int ti = tIdx(first[Xidx][l]);
                             if(table[i][ti][0] == '\0'){
                                 strcpy(table[i][ti],rhs);
                             }else{
@@ -248,7 +247,7 @@ void buildTable(){
                                 strcat(table[i][ti],rhs);
                             }
                         }
-                        if(firstSym == '#'){
+                        else{
                             hasEps = 1;
                         }
                     }
@@ -269,10 +268,10 @@ void buildTable(){
                     }
                 }
             }
-        
             if(rhsCanbeEps){
                 for(int f=0;follow[i][f];f++){
-                    int ti = tIdx(follow[i][f]);
+                    char b = follow[i][f];
+                    int ti = tIdx(b);
                     if(table[i][ti][0] == '\0'){
                         strcpy(table[i][ti],rhs);
                     }else{
@@ -288,35 +287,33 @@ void buildTable(){
 void printTable(){
     printf("%-8s","");
     for(int i=0;i<termCount;i++){
-        printf("%-12c",terms[i]);
+        printf("%-8c",term[i]);
     }
     printf("\n");
     for(int i=0;i<ntCount;i++){
         printf("%-8c",nt[i]);
         for(int j=0;j<termCount;j++){
             if(table[i][j][0] == '\0'){
-                printf("%-14s","-");
+                printf("%-8c",'-');
             }else{
-                printf("%c->%-11s",nt[i],table[i][j]);
+                printf("%c->%-8s",nt[i],table[i][j]);
             }
         }
         printf("\n");
     }
 }
 
-void inputTable(){
-    printf("Enter predictive parsing table entries: ");
+void takeFirst(){
     for(int i=0;i<ntCount;i++){
-        for(int j=0;j<termCount;j++){
-            char temp[50];
-            printf("M[%c,%c] = ",nt[i],terms[j]);
-            scanf("%s",temp);
-            if(strcmp(temp,"-") == 0){
-                table[i][j][0] = '\0';
-            }else{
-                strcpy(table[i][j],temp);
-            }
-        }
+        printf("first(%c) = ",nt[i]);
+        scanf("%s",first[i]);
+    }
+}
+
+void takeFollow(){
+    for(int i=0;i<ntCount;i++){
+        printf("follow(%c) = ",nt[i]);
+        scanf("%s",follow[i]);
     }
 }
 
@@ -331,28 +328,31 @@ void predictiveParse(){
     printf("Enter string to parse: ");
     scanf("%s",input);
     strcat(input,"$");
-
+    int ip = 0;
     char stack[MAX];
     int top = -1;
     stack[++top] = '$';
-    stack[++top] = nt[0];
-    int ip = 0;
-    printf("\n\n%-15s%-15s%-15s\n","STACK","INPUT","ACTION");
+    stack[++top] = start;
+
+    printf("%-10s%-10s%-10s\n","STACK","INPUT","ACTION");
 
     while(top>=0){
         char stackTop = stack[top];
-        char currentSymbol = input[ip];
+        char currentSym = input[ip];
+
         printStack(stack,top);
         printf("\t\t");
+
         printf("%s",input+ip);
         printf("\t\t");
-        if(stackTop == '$' && currentSymbol == '$'){
+
+        if(stackTop == '$' && currentSym == '$'){
             printf("ACCEPT\n");
             break;
         }
         if(!isupper(stackTop)){
-            if(stackTop == currentSymbol){
-                printf("MATCH %c\n",currentSymbol);
+            if(stackTop == currentSym){
+                printf("MATCH\n");
                 top--;
                 ip++;
             }else{
@@ -361,8 +361,9 @@ void predictiveParse(){
             }
         }else{
             int nti = ntIdx(stackTop);
-            int ti = tIdx(currentSymbol);
-            if(ti == -1 || ti == -1){
+            int ti = tIdx(currentSym);
+
+            if(nti == -1 || ti == -1){
                 printf("ERROR\n");
                 break;
             }
@@ -370,13 +371,13 @@ void predictiveParse(){
                 printf("ERROR\n");
                 break;
             }
-            char production[MAX];
-            strcpy(production,table[nti][ti]);
-            printf("%c->%s\n",stackTop,production);
+            char temp[MAX];
+            strcpy(temp,table[nti][ti]);
+            printf("%c->%s\n",stackTop,temp);
             top--;
-            if(strcmp(production,"#") != 0){
-                for(int i=strlen(production)-1;i>=0;i--){
-                    stack[++top] = production[i];
+            if(strcmp(temp,"#") != 0){
+                for(int i=strlen(temp)-1;i>=0;i--){
+                    stack[++top] = temp[i];
                 }
             }
         }
@@ -391,31 +392,32 @@ int main(){
         scanf("%s",prod[i]);
     }
 
-    // Calculate first and follow
+    // First and follow
     // getNonTerminals();
-
     // computeFirst();
     // printFirst();
-
     // computeFollow();
     // printFollow();
 
-    // Build LL(1) Table
+    // LL(1) table
     // getNonTerminals();
     // getTerminals();
-
     // takeFirst();
-    // printFirst();
     // takeFollow();
-
     // buildTable();
     // printTable();
 
     // Non recursive predictive parsing
-    getTerminals();
     getNonTerminals();
-    inputTable();
-    // printTable();
+    printf("Enter start symbol: ");
+    scanf(" %c",&start);
+
+    if(ntIdx(start) == -1){
+        printf("Invalid Start Symbol!\n");
+        return 0;
+    }
+    getTerminals();
+    tableInput();   
     predictiveParse();
 
     return 0;
